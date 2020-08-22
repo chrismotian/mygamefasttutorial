c1523ddf78858f5c653416bb3fda9928b1db959b
chrismotian <chrisjberlin@googlemail.com>
2020-08-22 09:59:01 +0200

    tinker non-playable agent

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ class MyApp(ShowBase):
         ShowBase.__init__(self)
         self.world = self.loader.loadModel("world.bam")
         self.world.reparentTo(self.render)
+
+        self.player = self.loader.loadModel("alliedflanker.egg")
+        self.player.setPos(0,500,65)
+        self.player.setH(-180)
+        self.player.setScale(100,100,100)
+        self.player.setR(45)
+        self.player.setP(45)
+        self.player.reparentTo(self.render)
         
 app = MyApp()
 app.run()
